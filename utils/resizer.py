8212764757e7604524702cd7cc188cf63965a084
BuildTools <rayanmaster50@gmail.com>
2024-10-31 01:42:52 +0500

    Added img resizer util

---
+++ utils/resizer.py
@@ -0,0 +1,10 @@
+from PIL import Image
+import os
+
+if __name__ == "__main__":
+  for file in os.listdir():
+    if file.endswith(".png"):
+      image = Image.open(file).convert("RGBA")
+      scaled_img = image.resize((100, 100), Image.NEAREST)
+      fn, ex = file.split(".")
+      scaled_img.save(fn + "_resized." + ex)
